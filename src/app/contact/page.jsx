b2f22fd82bd02efa397837c5d7abafce5701c832
unknown <cogitaciones1@gmail.com>
2024-09-13 23:04:23 -0300

    Contact

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -74,41 +74,45 @@ export const Contact = () => {
 
   return (
     <div className="w-dvw flex flex-col items-center p-3  gap-5">
-
-    <div className=" text-xl md:text-3xl mt-10">
-      <h1>Pongamonos en contacto!</h1>
-    </div>
-    <div className="text-center w-96">
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati vitae, possimus, laborum id impedit quia quos, veniam quasi ipsam doloribus omnis. Expedita explicabo culpa libero modi sint dicta dolor doloremque.</p>
-    </div>
-    <div className="flex w-dvw justify-center h-full items-center">
-
-      <form
-        ref={form}
-        onSubmit={sendEmail}
-        className="container max-w-lg flex flex-col gap-4 border border-slate-200 border-2 rounded px-5 py-12 shadow mx-10"
-      >
-        {inputs.map((input, i) => {
-          return (
-            <InputForm
-              handleChange={handleChange}
-              handleBlur={handleBlur}
-              input={input}
-              errors={errors}
-              setErrors={setErrors}
-              formValues={formValues}
-              setFormValues={setFormValues}
-              tagName={input.tagName}
-            />
-          );
-        })}
-        <input
-          type="submit"
-          value={buttonState}
-          className="border p-2 cursor-pointer rounded bg-[#b7cce3] hover:border-[#8eb2d8] active:border-[#80afe1] text-slate-700 focus:outline-none "
-        />
-      </form>
-    </div>
+      <div className=" text-xl md:text-3xl mt-10">
+        <h1>Pongamonos en contacto!</h1>
+      </div>
+      <div className="text-center w-96">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
+          vitae, possimus, laborum id impedit quia quos, veniam quasi ipsam
+          doloribus omnis. Expedita explicabo culpa libero modi sint dicta dolor
+          doloremque.
+        </p>
+      </div>
+      <div className="flex w-dvw justify-center h-full items-center">
+        <form
+          ref={form}
+          onSubmit={sendEmail}
+          className="container max-w-lg flex flex-col gap-4 border border-slate-200 border-2 rounded px-5 py-12 shadow mx-10"
+        >
+          {inputs.map((input, i) => {
+            return (
+              <InputForm
+                key={i}
+                handleChange={handleChange}
+                handleBlur={handleBlur}
+                input={input}
+                errors={errors}
+                setErrors={setErrors}
+                formValues={formValues}
+                setFormValues={setFormValues}
+                tagName={input.tagName}
+              />
+            );
+          })}
+          <input
+            type="submit"
+            value={buttonState}
+            className="border p-2 cursor-pointer rounded bg-[#b7cce3] hover:border-[#8eb2d8] active:border-[#80afe1] text-slate-700 focus:outline-none "
+          />
+        </form>
+      </div>
     </div>
   );
 };
